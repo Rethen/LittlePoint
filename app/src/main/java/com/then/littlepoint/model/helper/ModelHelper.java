9436fb31150add7965ed97f739dbb7436c2bb736
then <425241566@qq.com>
2016-02-24 16:50:31 +0800

    增加类ModelHelper

---
+++ app/src/main/java/com/then/littlepoint/model/helper/ModelHelper.java
@@ -0,0 +1,39 @@
+package com.then.littlepoint.model.helper;
+
+/**
+ * Created by 42524 on 2016/2/24.
+ */
+public class ModelHelper {
+
+    private int currentPage;
+
+    private int totelPage;
+
+    private String url;
+
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
+    }
+
+
+    public int getTotelPage() {
+        return totelPage;
+    }
+
+    public void setTotelPage(int totelPage) {
+        this.totelPage = totelPage;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
